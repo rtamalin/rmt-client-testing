3f24edc4964b17e1d8a0db0c14c5ffc1d4c79a17
Fergal Mc Carthy <fmccarthy@suse.com>
2025-08-21 12:51:46 -0400

    Add Makefile rules for testing purposes.
    The client-register rules will trigger building of the testing image,
    as well as generation of candidate client hwinfo data, and will then
    use the image to register those clients with the target RMT.
    
    The client-tester run can be used to start the testing image as an
    interactive session.
    
    Updated the entrypoint script message to indicate where to find the
    resources copied or mounted into the image.

--- Makefile
+++ Makefile
@@ -2,5 +2,9 @@
 CNTR_MGR = docker
 REPO_BASE_DIR := $(dir $(abspath $(firstword $(MAKEFILE_LIST))))
 
--include Makefile.docker
+# golang rules
 include Makefile.golang
+
+# optional rules that may not exist in all cases
+-include Makefile.docker
+-include Makefile.tester
--- entrypoint.bash
+++ entrypoint.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "RMT Client Tester"
+cat - << __EOF__
+RMT Client Tester
+=================
+
+The rmt-client-tester repo is mounted as /app/tester
+The generated ClientDataStore is mounted as /app/ClientDataStore
+The SUSE/connect-ng and rmt-client-tester built commands are available
+in /app/bin.
+__EOF__
 
 exec bash -i
